5ae873f740d2a895d52f68b6bd15120ef851d85b
Taylor Jones <taylorjonessoftware@gmail.com>
2017-08-18 10:31:36 -0400

    Messing with props

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -5,4 +5,4 @@ import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
