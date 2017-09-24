eeaca67ff8376db45ac04dfa12e1638b12237a06
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-09-23 20:44:12 -0400

    working through events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log('You changed the URL');
+    // First, grab the text from the box
+    console.log(this.storeInput);
+    // Second, we're going to transition to the things
+  }
+
   render() {
     // Regular comments look like this.
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* JSX Comments look like this */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
         <button type="submit">Visit Store</button>
       </form>
     )
