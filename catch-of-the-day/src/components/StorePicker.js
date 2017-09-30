ab6b74a2c1a36feed978826cab93cec174c32992
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-09-30 11:27:35 -0400

    adding in event handling

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  // One way to access the `this` context in React Components
+  // is by using a constructor.
+  //
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
   goToStore(event) {
     event.preventDefault();
     console.log('You changed the URL');
@@ -13,7 +22,7 @@ class StorePicker extends React.Component {
   render() {
     // Regular comments look like this.
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         { /* JSX Comments look like this */ }
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
